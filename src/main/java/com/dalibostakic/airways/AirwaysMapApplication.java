089d0064d720b4b3f88a8a6f16d7b92c226c8299
daliborstakic <stakicdalibor64@gmail.com>
2025-01-03 21:09:47 +0100

    chore: edit project structure and pom.xml

--- src/main/java/com/dalibostakic/airways/AirwaysMapApplication.java
+++ src/main/java/com/dalibostakic/airways/AirwaysMapApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirwaysMapApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AirwaysMapApplication.class, args);
 	}
-
 }
