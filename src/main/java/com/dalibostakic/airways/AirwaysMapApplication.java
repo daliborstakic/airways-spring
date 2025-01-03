1d3df1454f99938f2e4f64c25b7b54d0bf0dba98
daliborstakic <stakicdalibor64@gmail.com>
2025-01-03 16:01:54 +0100

    initial commint

---
+++ src/main/java/com/dalibostakic/airways/AirwaysMapApplication.java
@@ -0,0 +1,13 @@
+package com.dalibostakic.airways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwaysMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwaysMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalibostakic/airways/AirwaysMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalibostakic.airways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwaysMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
